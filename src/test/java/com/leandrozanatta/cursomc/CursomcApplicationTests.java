0e01f20a7c5201ecb9f5ca5d7fa6b6c0d03ad06d
Leandro Zanatta <leandrozanatta7@gmail.com>
2021-08-24 13:04:35 -0300

    Projeto criado

---
+++ src/test/java/com/leandrozanatta/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrozanatta.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
